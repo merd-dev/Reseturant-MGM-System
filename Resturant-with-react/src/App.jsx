2554cbd3e5fdc83f940a6f8d231ee8cb8d328f17
merdekiyos <amerdekiyos@gmail.com>
2025-03-21 09:48:08 +0300

    preparing working space for project

--- Resturant-with-react/src/App.jsx
+++ Resturant-with-react/src/App.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+
+import {AboutUs,Chef,FindUs,Footer,Gallery,Header,Intro,Laurels,SpecialMenu} from "./container";
+import {Navbar} from './components'
 import "./App.css";
 
+
+
 function App() {
-  return <></>;
+  return (
+    <>
+     <Navbar/>
+     <Header />
+     <AboutUs/>
+     <SpecialMenu/>
+     <Chef/>
+     <Intro/>
+     <Laurels/>
+     <Gallery/>
+     <FindUs/>
+     <Footer/>
+    </>
+  );
 }
 
 export default App;
