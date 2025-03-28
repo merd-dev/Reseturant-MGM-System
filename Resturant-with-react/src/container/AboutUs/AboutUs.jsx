53444d8cacf0aa06296af10ee74e91c47728565f
merdekiyos <amerdekiyos@gmail.com>
2025-03-28 17:51:49 +0300

    building about-section header

--- Resturant-with-react/src/container/AboutUs/AboutUs.jsx
+++ Resturant-with-react/src/container/AboutUs/AboutUs.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-   <h1 className='about'>hi merd</h1>
+  <div className="app-wrapper app__bg  section__padding" id="about">
+    <h1 className="app-h1">About Us</h1>
+  
+    <div className="app__text">
+      <p className="app-p">Write your best history with us</p>
+    </div>
   </div>
 );
 
